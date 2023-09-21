de8c46f1b69cf6f9846d6d96692bac905de2e5e0
吴博 <wubo_aaa@163.com>
2023-09-21 19:31:24 +0800

    去掉 @Primary

--- src/main/java/cn/wubo/multi/level/cache/config/MultiLevelCacheConfiguration.java
+++ src/main/java/cn/wubo/multi/level/cache/config/MultiLevelCacheConfiguration.java
@@ -1,18 +1,15 @@
 package cn.wubo.multi.level.cache.config;
 
 import cn.wubo.multi.level.cache.core.MultiLevelCacheManager;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableConfigurationProperties({MultiLevelCacheProperties.class})
 public class MultiLevelCacheConfiguration {
 
     @Bean("multiLevelCacheManager")
-    @Primary
     public MultiLevelCacheManager multiLevelCacheManager(MultiLevelCacheProperties multiLevelCacheProperties) {
         return new MultiLevelCacheManager(multiLevelCacheProperties);
     }
